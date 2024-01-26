cb37efa833672e13f97aeea21f39063b0caec9dd
Kam Kennedy <kameronk013@gmail.com>
2024-01-26 16:47:20 -0700

    sad path complete

--- app/controllers/Api/V0/customer_subscriptions_controller.rb
+++ app/controllers/Api/V0/customer_subscriptions_controller.rb
@@ -1,6 +1,6 @@
 class Api::V0::CustomerSubscriptionsController < ApplicationController
   def create
-    if Customer.find(params[:customer_id]).nil? || Subscription.find(params[:subscription_id]).nil?
+    if Customer.find_by(id: params[:customer_id]).nil? || Subscription.find_by(id: params[:subscription_id]).nil?
       render json: { error: 'Customer or Subscription not found' }, status: 404
     elsif CustomerSubscription.find_by(customer_id: params[:customer_id], subscription_id: params[:subscription_id])
       render json: { error: 'Customer Subscription already exists' }, status: 409
